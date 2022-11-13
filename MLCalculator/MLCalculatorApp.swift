610ff570abd02f991101c25409d7cbef3db4511a
elichartnett <elichartnett@icloud.com>
2022-11-13 08:15:27 -0600

    Initial Commit

---
+++ MLCalculator/MLCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLCalculatorApp.swift
+//  MLCalculator
+//
+//  Created by Eli Hartnett on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MLCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
